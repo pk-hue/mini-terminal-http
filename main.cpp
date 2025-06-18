a2c7f791177043b85180573086ae9ef24d8ac150
Pedro H <ph159.com@gmail.com>
2025-06-17 21:00:36 -0300

    adicionada modulação do projeto

--- main.cpp
+++ main.cpp
@@ -4,53 +4,47 @@
 
 // Função de callback para armazenar a resposta da requisição
 size_t write_callback(void* contents, size_t size, size_t nmemb, std::string* output) {
-    size_t totalSize = size * nmemb;
-    output->append((char*)contents, totalSize);
-    return totalSize;
+    output->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
-int main() {
-    std::string url;
+// Função para fazer requisição e tratamento da url
+std::string fazerRequisicaoGet(const std::string& url) {
+    CURL* curl = curl_easy_init();
+    std::string response;
 
-    std::cout << "====== Mini Terminal HTTP com libcurl ======\n";
-    std::cout << "Digite uma URL (ou 'sair' para encerrar):\n";
-
-    while (true) {
-        std::cout << "\nURL > ";
-        std::cin >> url;
-
-        if (url == "sair") {
-            break;
-        };
-
-
-        CURL* curl = curl_easy_init();
-        if (!curl) {
-            std::cerr << "Falha ao iniciar o curl.\n";
-            continue;
-        }
-
-        std::string response;
-
+    if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-        CURLcode res = curl_easy_perform(curl);
-
-        if (res == CURLE_OK) {
-            std::cout << "\nResposta do servidor:\n";
-            std::cout << response << "\n";
-        } else {
-            std::cerr << "\nErro na requisicao: " << curl_easy_strerror(res) << "\n";
-        }
-
+        if (CURLcode res = curl_easy_perform(curl); res != CURLE_OK) std::cout << "Erro: " << curl_easy_strerror(res);
         curl_easy_cleanup(curl);
+    }else {
+        std::cerr << "Erro ao inicializar o CURL" << '\n';
     }
-
-    curl_global_cleanup();
-    std::cout << "Encerrado.\n";
-    return 0;
+    return response;
 }
 
+//Função de inicialização e impressão da resposta no terminal
+void inicializaTerminal() {
+    std::string url;
+    while (true) {
+        std::cout << "URL >";
+        std::cin >> url;
 
+        if (url == "sair") {
+            break;
+        }
+
+        std::string response = fazerRequisicaoGet(url);
+        std::cout << "Resposta do Servidor: \n" << response << '\n';
+    }
+}
+
+int main() {
+    std::cout << "====== [ Mini Terminal HTTP com libcurl ] ======\n";
+    inicializaTerminal();
+    std::cout << "Encerrando programa!" << '\n';
+    return 0;
+}
